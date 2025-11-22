f34c913e96b01a9a79b2a3006c25a5437452ccaa
SamuelChidiebere1 <Benstudio334@gmail.com>
2025-11-22 21:20:36 +0100

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,40 @@
+  <footer class="iptv-footer">
+    <div class="container">
+      <div class="footer-grid">
+        <div class="footer-column">
+          <h3>Persona</h3>
+          <p>Faucibus ornare suspendisse sed nisigittis volutpat odio facilisis mauris amet massa velit scelerisque.</p>
+          <p style="margin-top: 30px;">Join our newsletter</p>
+        </div>
+        <div class="footer-column">
+          <h3>Site Menu</h3>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#subscription">Subscription Iptv</a></li>
+            <li><a href="#channel">Channel</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h3>Information</h3>
+          <ul>
+            <li><a href="#faq">FAQ</a></li>
+            <li><a href="#privacy">Privacy Policy</a></li>
+            <li><a href="#refund">Refund Policy</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h3>Contact us</h3>
+          <p>Phone: (+63) 555 1212</p>
+          <p>Fax: (+63) 555 0100</p>
+          <p style="margin-top: 20px;">Contact us at:<br><a href="mailto:info@mail.com">info@mail.com</a></p>
+        </div>
+      </div>
+      <div class="copyright">
+        <p>Copyright © <?php echo date("Y"); ?> - premireiptv</p>
+      </div>
+    </div>
+  </footer>
+  <?php wp_footer(); ?>
+</body>
+</html>
